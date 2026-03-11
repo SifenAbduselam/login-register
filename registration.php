8fb00450e7515212d886cf0153b26cd620719784
Sifen Abduselam <sifenabduselam7@gmail.com>
2026-03-11 14:17:34 -0700

    done registration

--- registration.php
+++ registration.php
@@ -23,14 +23,74 @@ $email = trim($_POST["email"]);
 $password = $_POST["password"];
 $repeatPassword = $_POST["repeat-password"];
 
+$errors = [];
 
 $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
 
 
+if(empty($fullname) || empty($email) || empty($password) || empty($repeatPassword)) {
 
+$errors[] = "All fields are required";
 
 }
+
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+
+$errors[]= "Email is invalid";
+}
+
+if(strlen($password) < 8) {
+
+$errors[] = "password must be atleast 8 characters";
+}
+
+if($password !== $repeatPassword) {
+    $errors[] = "password must match";
+}
+
+if(empty($errors)) {
+
+$sql = "SELECT * FROM users WHERE email = ?";
+
+$stmt = mysqli_prepare($conn, $sql);
+
+mysqli_stmt_bind_param($stmt, "s", $email);
+
+mysqli_stmt_execute($stmt);
+
+$result = mysqli_stmt_get_result($stmt);
+
+if(mysqli_num_rows($result)>0) {
+
+$errors[] = "Email already exisis";
+}
+
+else {
+
+$sql = "INSERT INTO users(fullname, email, password) VALUES(?, ?, ?)";
+
+$stmt = mysqli_prepare($conn,$sql);
+mysqli_stmt_bind_param($stmt, "sss", $fullname, $email, $password_hash);
+
+mysqli_stmt_execute($stmt);
+
+echo "<div class = 'alert alert-success'>Registration successful</div>";
+
+}
+
+}
+}
+ if(!empty($errors)){
+    foreach($errors as $error){
+        echo "<div class='alert alert-danger'>$error</div>";
+    }
+ }
+ 
+
+
+
+
 
 
 
@@ -40,7 +100,7 @@ $password_hash = password_hash($password, PASSWORD_DEFAULT);
 ?>
 
 
-        <form action="process_register.php" method="POST">
+        <form action="registration.php" method="POST">
             <div class="form-group">
             <input type="text" class="form-control" name="fullname" placeholder="Enter name">
             </div>
